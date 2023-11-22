3ddbf43b4ff4f6934a682883a71d1a1c76e58818
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2023-11-22 12:08:52 +0900

    .

--- chap8/homework.py
+++ chap8/homework.py
@@ -1,24 +1,21 @@
-# 사전 정의된 규격으로 저장된 파일에서 랜덤으로 문제를 불러와서 문제를 출제하고 정답을 맞추는 Trivia Challenge Game 프로그램을 구현
-# 기본 파일 규격은 다음 페이지와 같지만, cpickle이나 shelve 라이브러리를 이용하여 데이터를 dictionary 또는 list형태로 저장해도 상관 없음
-# 문제는 학생 스스로 만들어서 사용
+text_file = open("./homework.txt", "r")
+text = text_file.readlines()
+print("\t\tWlcome to Trivia Challenge!\n\n")
 
-# <title>
-# -------------------
-# <category>
-# <question>
-# <answer 1>
-# <answer 2>
-# <answer 3>
-# <answer 4>
-# <correct answer>
-# <explanation>
+def prob():
+    print(f"\t\t{text[0]}\n\n{text[1]}\n{text[2]}\n\t1 - {text[3]}\n\t2 - {text[4]}\n\t3 - {text[5]}\n\t4 - {text[6]}\n")
 
-# An Episode You Can't Refuse
-# On the Run With a Mammal
-# Let's say you turn state's evidence and need to "get on the lamb." If you wait /too long, what will happen?
-# You'll end up on the sheep
-# You'll end up on the cow
-# You'll end up on the goat
-# You'll end up on the emu
-# 1
+def answer():
+    num = input("What's your answer?: ")
+    print()
+    return right(num)
 
+def right(num):
+    if num == text[7]:
+        print("You are Genious!")
+    else:
+        print("Try again...!")
+        answer()
+
+prob()
+answer()
